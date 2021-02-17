bbb65f2c1caf027cfc95aab095d64bf5ecd8cfd9
WinglessFrame <gleb122002@gmail.com>
2021-02-17 22:58:09 +0300

    Forgot to switch serializer in swipes ^_^. Url like enpoint changed

--- GinderProject/GinderApp/api/urls.py
+++ GinderProject/GinderApp/api/urls.py
@@ -15,12 +15,12 @@ app_name = 'GinderApp'
 urlpatterns = [
     # main app endpoint
     path('swipes', SwipesAPIView.as_view(), name='swipes'),
+    path('swipes/like/<int:author_pk>', LikeUserPostAPIView.as_view(), name='like'),
     # profile manipulations
     path('profile', ProfileAPIView.as_view(), name='profile'),
     path('profile/clear', ClearViewedAPIView.as_view(), name='clear'),
     path('profile/matches', MatchesListAPIView.as_view(), name='matches'),
-    # likes and chats endpoints
-    path('like/<int:author_pk>', LikeUserPostAPIView.as_view(), name='like'),
+    # endpoints
     path('chat/<int:pk>', ChatAPIView.as_view(), name='chat'),
     path('chat/<int:pk>/message', SendChatMessageAPIView.as_view(), name='message')
 ]
