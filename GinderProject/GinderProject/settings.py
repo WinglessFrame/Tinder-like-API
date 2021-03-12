aca43dfcbdad06bee29438589d8e2678b27171c4
WinglessFrame <gleb122002@gmail.com>
2021-03-12 11:31:27 +0300

    Fixes for docker

--- GinderProject/GinderProject/settings.py
+++ GinderProject/GinderProject/settings.py
@@ -91,10 +91,10 @@ WSGI_APPLICATION = 'GinderProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': os.environ.get("POSTGRES_DATABASE"),
-        'USER': os.environ.get("DB_USER"),
-        'PASSWORD': os.environ.get("DB_PASSWORD"),
-        'HOST': 'db',
+        'NAME': os.environ.get("DB_NAME", "ginderdb"),
+        'USER': os.environ.get("DB_USER", "postgres"),
+        'PASSWORD': os.environ.get("DB_PASSWORD", "postgres"),
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
